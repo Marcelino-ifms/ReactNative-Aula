1bde97f5b18ec791518458e61c29968311928177
Marcelino-ifms <140651566+Marcelino-ifms@users.noreply.github.com>
2024-01-10 08:44:06 -0300

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Consultar" component={Consultar}></Stack.Screen>
+        <Stack.Screen name="Registrar" component={Registrar}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
